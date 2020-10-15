6c07919c22c1ba2db4a148b72dfcbf650772f9b3
Ubuntu <ubuntu@ip-172-31-5-250.eu-central-1.compute.internal>
2020-10-15 12:48:48 +0000

    more refactoring

--- src/index.js
+++ src/index.js
@@ -6,10 +6,18 @@ app.use(compression())
 
 const cheerio = require('cheerio')
 const template = require('./template.js')
+const { update, getGasInfo } = require('./utils.js')
 const { ETHERSCAN_APIKEY } = require('../config.js')
 
 const wrap = fn => (...args) => fn(...args).catch(args[2])
 const tail = (arr) => arr.slice(1)
+
+const providers = {
+  etherscan: `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_APIKEY}`,
+  poaNetwork: 'https://gasprice.poa.network',
+  myCrypto: 'https://gas.mycryptoapi.com/',
+  upvest: 'https://fees.upvest.co/estimate_eth_fees'
+}
 
 let data = {
   etherscan: [[],[],[]],
@@ -23,40 +31,14 @@ let data = {
   timestamp: 0
 }
 
-const getGasInfo = async (address, fields, path) => {
-  const response = await fetch(address)
-  const body = await response.json()
-  return [
-    path ? body[path][fields[0]] : body[fields[0]],
-    path ? body[path][fields[1]] : body[fields[1]],
-    path ? body[path][fields[2]] : body[fields[2]]
-  ].map(Math.round)
-}
-
-const update = (name) => {
-  if (data[name][0].length > 59) {
-    data[name] = data[name].map(a => tail(a))
-  }
-  data[name][0].push(data[name+"_current"][0])
-  data[name][1].push(data[name+"_current"][1])
-  data[name][2].push(data[name+"_current"][2])
-}
-
 const main = async () => {
-  data.etherscan_current = await getGasInfo(
-    `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${ETHERSCAN_APIKEY}`,
-    ['FastGasPrice', 'ProposeGasPrice', 'SafeGasPrice'],
-    'result'
-  )
-  data.poaNetwork_current = await getGasInfo('https://gasprice.poa.network', ['fast', 'standard', 'slow'])
-  data.myCrypto_current = await getGasInfo('https://gas.mycryptoapi.com/', ['fast', 'standard', 'safeLow'])
-  data.upvest_current = await getGasInfo('https://fees.upvest.co/estimate_eth_fees', ['fast', 'medium', 'slow'],'estimates')
+  data.etherscan_current = await getGasInfo(providers.etherscan, ['FastGasPrice', 'ProposeGasPrice', 'SafeGasPrice'],'result')
+  data.poaNetwork_current = await getGasInfo(providers.poaNetwork, ['fast', 'standard', 'slow'])
+  data.myCrypto_current = await getGasInfo(providers.myCrypto, ['fast', 'standard', 'safeLow'])
+  data.upvest_current = await getGasInfo(providers.upvest, ['fast', 'medium', 'slow'],'estimates')
   data.timestamp = Date.now()
   
-  update('etherscan')
-  update('poaNetwork')
-  update('myCrypto')
-  update('upvest')
+  Object.keys(providers).map(s => update(s, data))
 }
 
 main()
---
+++ src/utils.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch')
+
+const tail = (arr) => arr.slice(1)
+
+async function getGasInfo(address, fields, path) {
+  const response = await fetch(address)
+  const body = await response.json()
+  return [
+    path ? body[path][fields[0]] : body[fields[0]],
+    path ? body[path][fields[1]] : body[fields[1]],
+    path ? body[path][fields[2]] : body[fields[2]]
+  ].map(Math.round)
+}
+
+function update(name, data) {
+  if (data[name][0].length > 59) {
+    data[name] = data[name].map(a => tail(a))
+  }
+  data[name][0].push(data[name+"_current"][0])
+  data[name][1].push(data[name+"_current"][1])
+  data[name][2].push(data[name+"_current"][2])
+}
+
+module.exports = {
+  getGasInfo,
+  update
+}
