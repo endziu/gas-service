abdb7683e19846f96bf79d31dd3a2ff14b51fa77
Ubuntu <ubuntu@ip-172-31-5-250.eu-central-1.compute.internal>
2020-10-15 23:18:19 +0000

    declutter ui

--- src/template.js
+++ src/template.js
@@ -45,7 +45,7 @@ table { border-collapse: collapse; border-spacing: 0; }
   .md-grid-rows-2 { grid-template-rows: repeat(2, minmax(0, 1fr)); }
 }
 
-.max-width { max-width: 64rem; }
+.max-width { max-width: 96rem; }
 .bg-light-gray { background: #dddfdd; }
 .near-black { color: #222; }
 .fs-1 { font-size: 0.75rem; }
@@ -67,6 +67,7 @@ table { border-collapse: collapse; border-spacing: 0; }
 .block-center { margin: 1rem auto; }
 .skew-l { transform: perspective(800px) rotateY(-8deg); }
 .skew-r { transform: perspective(800px) rotateY(8deg); }
+.scale-up { transform: scale(1.1) }
 .shadow { 
   box-shadow: 
     rgba(0, 0, 0, 0.01) 0px 0px 0px 1px,
@@ -76,7 +77,7 @@ table { border-collapse: collapse; border-spacing: 0; }
 }
 .transition { transition: all 1s; }
 .bg-gray { background: #efefef; }
-.main:hover { transform: rotateY(0); box-shadow: none; transform: scale(1.1) }
+.main:hover { transform: rotateY(0); box-shadow: none }
 </style>`
 
 const template = (values) => `<!DOCTYPE html>
@@ -88,106 +89,46 @@ const template = (values) => `<!DOCTYPE html>
 </head>
 ${style}
 <body class="bg-light-gray">
-<h1 class="text-center m-3">
-  <svg viewBox="0 0 20 20" width="48px" stroke="black" stroke-width="1" fill="black" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <h1 class="text-center m-3">
+    <svg viewBox="0 0 20 20" width="48px" stroke="black" stroke-width="1" fill="black" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
     <path d="M13,18 L13,1.99079514 C13,0.898212381 12.1007504,0 10.9914698,0 L3.0085302,0 C1.90195036,0 1,0.891309342 1,1.99079514 L1,18 L0,18 L0,20 L14,20 L14,18 L13,18 Z M3,2 L11,2 L11,8 L3,8 L3,2 Z M13,10 L13.9989566,10 C15.1041023,10 16,10.8982606 16,11.9979131 L16,15.009222 C16,15.5564136 16.4438648,16 17,16 C17.5522847,16 18,15.5490248 18,15.009222 L18,10 L16,8 L16,6 L14,4 L15,3 L20,8 L20,14.9996703 C20,16.6567066 18.6534829,18 17,18 C15.3431458,18 14,16.6534829 14,15 L14,12 L13,12 L13,10 Z" id="Combined-Shape"></path>
-  </svg>
-</h1>
-<div class="grid grid-flow-col grid-cols-1 md-grid-cols-2 grid-rows-4 md-grid-rows-2 gap-4 p-2 max-width m-auto">
-  <div class="main bg-gray block-center fitToContent skew-r transition shadow">
-    <table class="fs-3 sans-serif near-black">
-    <tr>
-      <th colspan="2" class="border-dashed text-centered p-2 bold">etherscan</th>
-    </tr>
-    <tr>
-      <th class="border-dashed text-center p-2 bold">speed</th>
-      <th class="border-dashed text-center p-2 bold">price</th>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚀</td>
-      <td class="border-dashed text-center p-2" id="ets_fast">${values[0][0]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🏎️</td>
-      <td class="border-dashed text-center p-2" id="ets_average">${values[0][1]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚊</td>
-      <td class="border-dashed text-center p-2" id="ets_cheap">${values[0][2]} Gwei</td>
-    </tr>
-    </table>
+    </svg>
+  </h1>
+  <div class="max-width m-auto">
+    <div class="main bg-gray block-center fitToContent transition shadow">
+      <table class="fs-2 sans-serif near-black">
+        <tr>
+          <th class="border-dashed text-centered p-2"></th>
+          <th colspan="2" class="border-dashed text-centered p-2 bold">etherscan</th>
+          <th colspan="2" class="border-dashed text-centered p-2 bold">poaNetwork</th>
+          <th colspan="2" class="border-dashed text-centered p-2 bold">mycrypto</th>
+          <th colspan="2" class="border-dashed text-centered p-2 bold">upvest</th>
+        </tr>
+        <tr>
+          <td class="border-dashed text-center p-2">🚀</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="ets_fast">${values[0][0]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="poa_fast">${values[1][0]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="myc_fast">${values[2][0]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="upv_fast">${values[3][0]}</td>
+        </tr>
+        <tr>
+          <td class="border-dashed text-center p-2">🏎️</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="ets_average">${values[0][1]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="poa_average">${values[1][1]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="myc_average">${values[2][1]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="upv_average">${values[3][1]}</td>
+        </tr>
+        <tr>
+          <td class="border-dashed text-center p-2">🚊</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="ets_cheap">${values[0][2]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="poa_cheap">${values[1][2]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="myc_cheap">${values[2][2]}</td>
+          <td colspan="2" class="border-dashed text-center p-2" id="upv_cheap">${values[3][2]}</td>
+        </tr>
+      </table>
+    </div>
   </div>
-  <div class="main bg-gray block-center fitToContent skew-r transition shadow">
-    <table class="fs-3 sans-serif near-black">
-    <tr>
-      <th colspan="2" class="border-dashed text-centered p-2 bold">POA Network</th>
-    </tr>
-    <tr>
-      <th class="border-dashed text-center p-2 bold">speed</th>
-      <th class="border-dashed text-center p-2 bold">price</th>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚀</td>
-      <td class="border-dashed text-center p-2" id="poa_fast">${values[1][0]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🏎️</td>
-      <td class="border-dashed text-center p-2" id="poa_average">${values[1][1]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚊</td>
-      <td class="border-dashed text-center p-2" id="poa_cheap">${values[1][2]} Gwei</td>
-    </tr>
-    </table>
-  </div>
-  <div class="main bg-gray block-center fitToContent skew-l transition shadow">
-    <table class="fs-3 sans-serif near-black">
-    <tr>
-      <th colspan="2" class="border-dashed text-centered p-2 bold">myCrypto</th>
-    </tr>
-    <tr>
-      <th class="border-dashed text-center p-2 bold">speed</th>
-      <th class="border-dashed text-center p-2 bold">price</th>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚀</td>
-      <td class="border-dashed text-center p-2" id="myc_fast">${values[2][0]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🏎️</td>
-      <td class="border-dashed text-center p-2" id="myc_average">${values[2][1]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚊</td>
-      <td class="border-dashed text-center p-2" id="myc_cheap">${values[2][2]} Gwei</td>
-    </tr>
-    </table>
-  </div>
-  <div class="main bg-gray block-center fitToContent skew-l transition shadow">
-    <table class="fs-3 sans-serif near-black">
-    <tr>
-      <th colspan="2" class="border-dashed text-centered p-2 bold">upvest</th>
-    </tr>
-    <tr>
-      <th class="border-dashed text-center p-2 bold">speed</th>
-      <th class="border-dashed text-center p-2 bold">price</th>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚀</td>
-      <td class="border-dashed text-center p-2" id="upv_fast">${values[3][0]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🏎️</td>
-      <td class="border-dashed text-center p-2" id="upv_average">${values[3][1]} Gwei</td>
-    </tr>
-    <tr>
-      <td class="border-dashed text-center p-2">🚊</td>
-      <td class="border-dashed text-center p-2" id="upv_cheap">${values[3][2]} Gwei</td>
-    </tr>
-    </table>
-  </div>  
-</div>
-<p id="timer" class="text-center sans-serif fs-1 m-3">last update: just now.</p>
+  <p id="timer" class="text-center sans-serif fs-1 m-3">last update: just now.</p>
 <script type="text/javascript">
   function getCurrentData() {
     fetch('http://18.192.6.19/history')
